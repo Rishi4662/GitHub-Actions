9c656bc0eb5d3613809e83fab79cccb4e6139310
Rishi Raj <rishi.raj4662@gmail.com>
2023-08-30 16:18:35 +0530

    Adding AZ

--- main.tf
+++ main.tf
@@ -27,6 +27,8 @@ resource "aws_vpc" "example_vpc" {
 resource "aws_subnet" "example_subnet" {
   vpc_id     = aws_vpc.example_vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "ap-south-1a"
 }
 
 resource "aws_security_group" "example_security_group" {
